19d35928355d6d0de5f8b40901d3b6f980c4482a
Jakov4848 <jakov4848@gmail.com>
2024-10-30 22:25:36 +0100

    added book one

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace LibraryManagementSystem
         static void Main(string[] args)
         {
             Book book0 = new Book();
+            Book book1 = new Book();
             System.Console.WriteLine(book0.ID);
             System.Console.WriteLine(Book.BookCount);
         }
